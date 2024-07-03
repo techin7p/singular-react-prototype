be8418d44ee18570fb0c87c0229d52b5661f299a
Techin Supapol <title.spp@gmail.com>
2024-07-03 13:00:16 +0700

    feat: update websocket

---
+++ src/hooks/useSubscriptions.ts
@@ -0,0 +1,68 @@
+import { useState } from "react"
+import { removeSubscription, subscribe } from "../helpers/eventsub/subscribe"
+import useWebSocket from "react-use-websocket"
+
+const subscriptions = [
+  {
+    type: "channel.follow",
+    version: "2",
+    condition: {
+      broadcaster_user_id: "731867899",
+      moderator_user_id: "731867899",
+    },
+  },
+  // {
+  //   type: "channel.subscribe",
+  //   version: "1",
+  //   condition: {
+  //     broadcaster_user_id: "731867899",
+  //   },
+  // },
+  {
+    type: "channel.chat.message",
+    version: "1",
+    condition: {
+      broadcaster_user_id: "731867899",
+      user_id: "731867899",
+    },
+  },
+]
+
+export const useSubscriptions = () => {
+  const [subIds, setSubIds] = useState<string[]>([])
+
+  const { lastJsonMessage, readyState } = useWebSocket("wss://eventsub.wss.twitch.tv/ws?keepalive_timeout_seconds=60", {
+    onMessage: async (event) => {
+      console.log("Message from server ", event.data)
+      const data = JSON.parse(event.data)
+      if (data?.metadata?.message_type === "session_welcome") {
+        const sessionId = data.payload.session.id
+        await subscribeAll(sessionId)
+      }
+    },
+    onClose: () => {
+      console.log('close!!')
+      unsubscribeAll()
+    },
+    shouldReconnect: () => true
+  });
+
+  const subscribeAll = async (sessionId: string) => {
+    const subIds = []
+    for (const sub of subscriptions) {
+      const data = await subscribe({...sub, sessionId})
+      console.log(data)
+      subIds.push(data.data[0].id)
+    }
+    console.log(subIds)
+    setSubIds(subIds)
+  }
+
+  const unsubscribeAll = async () => {
+    for (const id of subIds) {
+      await removeSubscription(id)
+    }
+  }
+
+  return { lastJsonMessage, readyState };
+}
